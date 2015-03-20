fc4a4794f220386867514163d4a75a9b442b2a60
Jordan Parra <japarra4@hotmail.com>
2015-03-20 17:12:16 +0000

    eh

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ $(function() {
   $("input[type=\"reset\"]").on("click", function() {
      document.getElementById("name").focus()
   });
+  
+  $(".message-control").on("focus", function() {
+      $("#messages").html("<div>" + $(this).attr("title") + "</div>");
+  });
 });
 
 // document.getElementById("external-nav-links").innerHTML("<p>Hello Bitches</p>");
