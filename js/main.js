46e8b13068f4474830e52bc7f2e7edaab9c53012
Jordan Parra <japarra4@hotmail.com>
2015-03-20 17:23:33 +0000

    eh

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function() {
+  //
+  $("#messages").html("<div>" + $("#name").attr("title") + "</div>");
+  
+  // Add a hover effect to the external link images
   $(".image-link").hover( 
       function() {
         $(this).css("background", "rgba(0,0,0,0.1)");
@@ -7,13 +11,13 @@ $(function() {
       }
   ); 
   
+  // Give focus to the first field on the form after hitting the reset button
   $("input[type=\"reset\"]").on("click", function() {
-     document.getElementById("name").focus()
+     document.getElementById("name").focus();
   });
   
+  // Display a help message that utilizes the title attribute of the input field
   $(".message-control").on("focus", function() {
       $("#messages").html("<div>" + $(this).attr("title") + "</div>");
   });
 });
-
-// document.getElementById("external-nav-links").innerHTML("<p>Hello Bitches</p>");
